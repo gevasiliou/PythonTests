a03fe3fe00d154405c3cecb49215f1573def6896
gevasiliou <ge.vasiliou@gmail.com>
2016-10-06 17:38:08 +0300

    Update click.py

--- click.py
+++ click.py
@@ -2,13 +2,12 @@ from pymouse import PyMouse
 from time import sleep
 m = PyMouse()
 x, y = m.position()
-print('current:',x,y)
-#m.click(x,y,2)
 a, b = m.screen_size()
-print(a,b)
-print(a/2,b/2)
-sleep(5)
-m.move(a/2,b/2)
-m.click(a/2,b/2,2)
-## this extra line added for testing  with git synchronization (local file updates remote file)
-## this line added for the reverse update. Change in remote file must update local file.
+print('current position:',x,y)
+print('screen dimensions:', a, b)
+sleep(10) #wait 10 seconds
+#m.click(x,y,2) #With this line, right click happens wherever your mouse is 
+
+m.move(a/2,b/2) #With this line your mouse pointer is moved on the center of the screen
+m.click(a/2,b/2,2) #and right click is injected in the center of the screen.
+#moving the mouse pointer is not mandatory for m.click to work.
