b79d5b0deaf17bbe2f9425709ef72ae622c46951
root <root@debian>
2016-10-06 16:11:41 +0300

    click.py

---
+++ click.py
@@ -0,0 +1,12 @@
+from pymouse import PyMouse
+from time import sleep
+m = PyMouse()
+x, y = m.position()
+print('current:',x,y)
+#m.click(x,y,2)
+a, b = m.screen_size()
+print(a,b)
+print(a/2,b/2)
+sleep(5)
+m.move(a/2,b/2)
+m.click(a/2,b/2,2)
