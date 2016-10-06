e4bacd2bcfad0022732026409bc14b8932034e74
gevasiliou <ge.vasiliou@gmail.com>
2016-10-06 12:35:01 +0300

    Create devlist.py

---
+++ devlist.py
@@ -0,0 +1,8 @@
+# Sample Code using python-evdev tutorials to list properties of all devices.
+from evdev import InputDevice, list_devices
+from pprint import pprint #the pretty print modules
+for dev in list_devices(): # scan all devices
+   device = InputDevice(dev)
+   pprint(('device: ', device,'-',device.name))
+   cap = device.capabilities(verbose=True,absinfo=True)
+   pprint(('Device Capabilities:', cap))
