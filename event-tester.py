15943bb478b4b63bbc31d124a1ade9c0e5d345ea
root <root@debian.org>
2016-11-29 18:16:50 +0200

    update

--- event-tester.py
+++ event-tester.py
@@ -1,6 +1,6 @@
 from evdev import UInput, AbsInfo
 #from evdev import ecodes as e
-from evdev import InputDevice, categorize, ecodes 
+from evdev import InputDevice, categorize, ecodes, resolve_ecodes, resolve_ecodes_dict, AbsEvent, InputEvent
 import time
 from datetime import datetime
 from pymouse import PyMouse
@@ -11,7 +11,7 @@ def stamp():
 	return stamp;
 
 rep=1
-
+'''
 while rep == 1 :
 	print stamp(), ' Lets start this shit'
 	time.sleep(2)
@@ -23,12 +23,13 @@ while rep == 1 :
 	m.click(x, y, 2)  # the third argument represents the mouse button (1 left click,2 right click,3 middle click)
 
 	print stamp(), 'right click injected at ', m.position()
-
 '''
-dev = InputDevice('/dev/input/event10')
+
+#dev = InputDevice('/dev/input/event10') #Event10 is the ELAN in Toshiba (run evtest to verify)
+dev = InputDevice('/dev/input/event6') #Event6 is the Virtual Box Mouse in HP
+
 print(dev)
 for event in dev.read_loop():
-    if event.type == ecodes.EV_ABS:
-#       print(categorize(event))
-		print (event,"---",event.value)
-'''
+	if event.type == ecodes.EV_ABS: #With this IF you can limit what to be printed. Remove it to print everything.
+#		print(categorize(event))
+		print AbsEvent(event),'-->','Event TimeStamp:', event.timestamp(), 'Event Code: ', event.code, ' Event Type: ',event.type, ' Event Value: ',event.value
