175114dfeab6e2c9e85018bbaa179492e88d27ae
gevasiliou <ge.vasiliou@gmail.com>
2016-12-02 03:25:30 +0200

    update

--- rightclick-gv.py
+++ rightclick-gv.py
@@ -8,17 +8,14 @@ import subprocess
 def rightclick():
     print "Right Click Timer Starting. Pressed Flag=", pressed
     timestop = time.time()
-    x1, y1 = m.position()  # Get the coordinates after time has elapsed
-    endx = x1
-    endy = y1
+    endx, endy = m.position()  # Get the coordinates after time has elapsed
     deltatime = timestop - timestart
-    print 'Timer Finished. Pressed Flag:', pressed, 'Deltatime=', deltatime, ' Delta X:', abs(
-        endx - startx), ' Delta Y:', abs(endy - starty)
     if pressed == True and (abs(endx - startx) < 20) and (abs(endy - starty) < 20):
-        subprocess.check_call(['xinput', '--disable', 'ELAN Touchscreen'])
-        m.press(x1, y1, 2)
-        m.release(x1, y1, 2)
-        subprocess.check_call(['xinput', '--enable', 'ELAN Touchscreen'])
+		print 'Timer Finished - Right Click to be injected. Pressed Flag:', pressed, 'Deltatime=', deltatime, ' Delta X:', abs(endx - startx), ' Delta Y:', abs(endy - starty)
+		subprocess.check_call(['xinput', '--disable', 'ELAN Touchscreen'])
+		m.press(endx, endy, 2)
+		m.release(endx, endy, 2)
+		subprocess.check_call(['xinput', '--enable', 'ELAN Touchscreen'])
 #       Bug: You can not have right click context menu on multiple sellected items inside Nautilus
 #       Workaround: For multiple selected items in Nautilus you can bring context menu with two finger tap
 
@@ -30,23 +27,22 @@ m = PyMouse()
 
 pressed = False
 for event in dev.read_loop():
-    #	print categorize(event),'-->','Event TimeStamp:', event.timestamp(), 'Event Code: ', event.code, ' Event Type: ',event.type, ' Event Value: ',event.value
     if event.type == 1 and event.code == 330 and event.value == 1:
-        x, y = m.position()  # gets mouse current position coordinates
+        startx, starty = m.position()  # gets mouse current position coordinates
         pressed = True
         timestart = time.time()
-        startx = x
-        starty = y
         t = Timer(1.0, rightclick)
         t.start()
 
-    # Bellow i detect the finger relase (BTN_TOUCH Value becomes 0). If code bellow is enabled, cancels the right click injection.
     if event.type == 1 and event.code == 330 and event.value == 0:
-        print "Finger removed - cancelling..."
+    # Detecting finger release (BTN_TOUCH Value becomes 0). If code bellow is enabled, cancels the right click injection.
+        print "Finger removed ..."
         pressed = False
         t.cancel()
 
     if event.type == 3 and event.code == 47 and event.value == 1: #ABS_MT_SLOT EVENT. VALUE 1 = 2 FINGERS
+    # Two fingers tap brings right click menu. 
+    # Bug : Works only the first time in Nautilus
         print "Two Finger tap..."
         x2, y2 = m.position()  # Get the pointer coordinates
         m.click(x2, y2, 2)
