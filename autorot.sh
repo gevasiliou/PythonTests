e5d027dd6b243a2e1ea262f2ad12e7847609c94e
gevasiliou <ge.vasiliou@gmail.com>
2016-12-10 03:32:12 +0200

    update

--- autorot.sh
+++ autorot.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 #--button="Enter" --button="unlock" --button="exit" \
 #https://sourceforge.net/p/yad-dialog/wiki/Examples/
+# Source: https://linuxappfinder.com/blog/auto_screen_rotation_in_ubuntu
+# Tweaks: https://gist.github.com/mildmojo/48e9025070a2ba40795c
+
+TOUCHSCREEN='ELAN Touchscreen' #as reported by evtest
+TRANSFORM='Coordinate Transformation Matrix'
 
 function gv()
 {
-echo 'Start of Function gv'
-echo 'Parameters Received:' $1 $2 $3 #Parameters inside function get a different number / name.
+echo 'Start of Function gv. Parameters Received:' $1 $2 $3 #Parameters inside function get a different number / name.
  
 screen=$(xrandr | grep 'connected' |grep -v 'disconnected' | grep -oE '[a-zA-Z]+[\-]+[^ ]') 
 echo 'Auto Recognize screen {alt}:' $screen
@@ -18,28 +22,32 @@ do
 		case "$ORIENTATION" in
 		normal)
 		echo 'xrandr rotate normal'
-		xrandr --output $screen --rotate normal 
+		xrandr --output $screen --rotate normal
+		xinput set-prop "$TOUCHSCREEN" "$TRANSFORM" 1 0 0 0 1 0 0 0 1
 		#gsettings set com.canonical.Unity.Launcher launcher-position Left
 		;;		
 		bottom-up)
 		echo 'xrandr rotate bottom-up'
-		xrandr --output $screen --rotate inverted 
+		xrandr --output $screen --rotate inverted
+		xinput set-prop "$TOUCHSCREEN" "$TRANSFORM" -1 0 1 0 -1 1 0 0 1 
 		#gsettings set com.canonical.Unity.Launcher launcher-position Left
 		;;		
 		right-up)
 		echo 'xrandr rotate right-up'
-		xrandr --output $screen --rotate right 
+		xrandr --output $screen --rotate right
+		xinput set-prop "$TOUCHSCREEN" "$TRANSFORM" 0 1 0 -1 0 1 0 0 1
 		#gsettings set com.canonical.Unity.Launcher launcher-position Bottom
 		;;		
 		left-up)
 		echo 'xrandr rotate left-up'
-		xrandr --output $screen --rotate left 
+		xrandr --output $screen --rotate left
+		xinput set-prop "$TOUCHSCREEN" "$TRANSFORM" 0 -1 1 1 0 0 0 0 1
 		#gsettings set com.canonical.Unity.Launcher launcher-position Bottom
 		;;
 		esac
 done
 echo 'End of Function gv' 
-exit 0
+exit 1
 }
 
 function notifystop ()
@@ -51,7 +59,7 @@ exec 2> /dev/null
 pkill -f "monitor-sensor"
 pkill -f "monitor-sensor >> sensor.log 2>&1"
 pkill -f "inotifywait -e modify sensor.log"
-exit 0
+exit 1
 }
 
 function byebye ()
@@ -65,7 +73,7 @@ pkill -f "monitor-sensor"
 pkill -f "monitor-sensor >> sensor.log 2>&1"
 pkill -f "inotifywait -e modify sensor.log"
 pkill -f "yad --notification --command="./yadmenu.sh""
-exit 0
+exit 1
 }
 
 # ------------------------------------------------------- The main program ----------------------------- #
@@ -95,18 +103,14 @@ case $action in
 		else
 			echo "already running , skipping command"
 		fi
-		;; 
+		;;
     Lock*) 
 		echo 'Locked Selected'
-		notifystop
-		;;
+		notifystop;;
     Exit*)
 		echo 'Exit Selected' 
-		byebye
-		;;
-	    *) 
-		exit 0 
-		;;    
+		byebye;;
+	    *) exit 0;;    
 esac
 echo 'Main Programm End'
 exit 0
