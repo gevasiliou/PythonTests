c684cb99a80417dd089ba96120c06e49d2ac1682
root <root@debian.org>
2016-12-13 15:28:40 +0200

    update

--- cliargs.py
+++ cliargs.py
@@ -8,9 +8,10 @@ print 'arguments found:', len(sys.argv)
 print 'command line arguments', sys.argv
 #cmdargs = str(sys.argv)
 #try:
+validflags=0
 if len(sys.argv) > 1:
-	if sys.argv[1]=="--debug": #this particulary checks arg1.
-		print 'Debug arg recevied in position 1'
+	#if sys.argv[1]=="--debug": #this particulary checks arg1.
+		#print 'Debug arg recevied in position 1'
 		#cmd='echo Debug option selected'
 		#q=subprocess.Popen(cmd,shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 		#out = q.communicate()[0]
@@ -18,11 +19,18 @@ if len(sys.argv) > 1:
 	#if "--debug" or "--dbg" in str(sys.argv): #this works but will catch debug even if it is part of another word (i.e --gtkdebug)
 	#	print '--debug arg received somewhere within args' 
 	for arg in sys.argv: 
-		if arg=="--debug":
-			print '--debug arg received somewhere within args'	
-
-else:
-	print 'You sent no extra args other than script name=arg[0]'
+		if arg==sys.argv[0]:
+			print "ignoring scripts name"
+		elif arg=="--nfr":
+			print '--NFR arg received somewhere within args'
+			validflags=validflags+1
+		elif arg=="--pymouse":	
+			print "Pymouse arg received somewhere"
+			validflags=validflags+1
+		else:
+			print arg,"is not a valid arg"
+if len(sys.argv)==1 or validflags==0:
+		print "No Valid flags sent at all. Default Values will be used"
 
 #except IndexError:
 #    pass
