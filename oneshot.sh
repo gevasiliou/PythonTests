32662f40a1fffbcb4e601a107de62b138a9a1a06
gevasiliou <ge.vasiliou@gmail.com>
2017-01-18 02:14:51 +0200

    update

--- oneshot.sh
+++ oneshot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Just an echo - nothing more to be done here"
 echo "Parameter received="$#
-[[ -n $1 ]] && echo "parameter 1 is emty"
+[[ -z $1 ]] && echo "parameter 1 is emty"
 exit 0
