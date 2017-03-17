f47b0bcc4b7edac04a26da696d6406655f55ea35
root <root@debian.org>
2017-03-17 18:04:21 +0200

    update

--- logit.sh
+++ logit.sh
@@ -1,6 +1,7 @@
+#! /bin/bash -i
 exec 3>&2 2>log 4<log 
 
 set -vx
 "$@" >&2
 set +vx 
-cat <&4 >&1  |tail -n +2 |head -n -2 #this prints out the whole logfile. If this is missing you see nothing on screen / command line
+cat <&4 >&1 # |tail -n +2 |head -n -2 #this prints out the whole logfile. If this is missing you see nothing on screen / command line
