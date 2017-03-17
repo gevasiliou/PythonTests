c05ff17a1d93bc10a9a22c52a847b95b84344aca
root <root@debian.org>
2017-03-17 18:09:03 +0200

    update

--- logit.sh
+++ logit.sh
@@ -5,3 +5,12 @@ set -vx
 "$@" >&2
 set +vx 
 cat <&4 >&1 # |tail -n +2 |head -n -2 #this prints out the whole logfile. If this is missing you see nothing on screen / command line
+
+#Alternative:
+#http://stackoverflow.com/questions/42841662/command-redirection/42847799#42847799
+# Make a function in your current shell / bash aliases
+#
+# $ function logme { a="$@"; echo $a >log ; "$@" >>log 2>&1;cat log;return; }
+# $ logme pwd  
+# > pwd
+# > /home/gv/Desktop/PythonTests
