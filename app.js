2478c6bc4b196ed30332b8ca717a543bf7782f18
root <root@debian>
2017-05-05 00:26:43 +0300

    update

---
+++ app.js
@@ -0,0 +1,14 @@
+var app = angular.module('flapperNews', []);
+
+app.controller('MainCtrl', [
+'$scope',
+function($scope){
+  $scope.test = 'Hello world!';
+  $scope.posts = [
+  'post 1',
+  'post 2',
+  'post 3',
+  'post 4',
+  'post 5'
+];
+}]);
