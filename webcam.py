ee0501cbcccf42b0ceed7ff197d16f9313369873
root <root@debian>
2017-09-18 23:02:19 +0300

    update

---
+++ webcam.py
@@ -0,0 +1,16 @@
+import pygame #pip install pygame is required first to be run at bash
+import pygame.camera
+import Image #used for display only - not needed for caption
+# http://www.pygame.org/docs/tut/CameraIntro.html
+
+pygame.camera.init()
+#pygame.camera.list_camera() #Camera detected or not
+cam = pygame.camera.Camera("/dev/video0",(640,480))
+cam.start()
+img = cam.get_image()
+pygame.image.save(img,"filename.jpg")
+cam.stop() #can be ommited - cam will be closed when program is finished
+#End of Caption
+
+image = Image.open('filename.jpg')
+image.show() #this calls imagemagick to display the image
