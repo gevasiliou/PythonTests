c51d41c395ba49fb717f20443e2600ac0fe9edf5
gevasiliou <ge.vasiliou@gmail.com>
2018-02-16 23:49:06 +0200

    update

--- kalitools/kalitools.sh
+++ kalitools/kalitools.sh
@@ -3,4 +3,4 @@ apt-get install aircrack-ng #wifi cracker - sniffer
 apt-get install wireshark #tcp sniffer
 apt-get install john #john the ripper -hash brute force tool
 apt install hydra #services login cracker
-
+dpkg -i ipscan_3.5.2_amd64.deb #Angry IP Scanner 64bit -/usr/bin/ipscan
