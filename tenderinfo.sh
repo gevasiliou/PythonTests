f3c8d9730e0e6c7187593a8467114187d385e996
George Vasiliou <ge.vasiliou@gmail.com>
2018-03-19 18:08:43 +0200

    Update tenderinfo.sh

--- tenderinfo.sh
+++ tenderinfo.sh
@@ -3,11 +3,11 @@ echo "Tender Info Engine"
 [[ -z $1 ]] && echo "pass me a valid tender info url" && exit 1
 u="$1"
 echo "URL to search tenders:  $1"
-keys="[Σσ]υσσω|ΣΥΣΣΩ|ΜΠΑΤ|[Μμ]πατ|UPS"
+keys="[Σσ]υσσω|ΣΥΣΣΩ|ΜΠΑΤ|[Μμ]πατ|[Uu][Pp][Ss]|[φΦ]ορτ|[αΑ]νορ|[Σσ]υστ[^ηήΗ]"
 read -e -p "Enter Regex Search Pattern (or press enter to use default keys):  " -i "$keys" keys
 echo "keys to search : $keys"
 
 echo "Starting"
-curl --silent "$u" |egrep "$keys" |perl -pe 's/>/>\n/g' |egrep -v '<u.*>|</a>' |sed 's/<a href="//g' |perl -pe 's/" style=.*>\n/ /g'
+curl --silent "$u" |egrep "$keys" |perl -pe 's/>/>\n/g' |egrep -v '<u.*>|</a>' |sed 's/<a href="//g' |perl -pe 's/" style=.*>\n/\n/g'
 echo "Finished"
 exit 0
