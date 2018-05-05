786b771254ce78fc032651890503441016b8dcb1
gevasiliou <ge.vasiliou@gmail.com>
2018-05-05 04:14:36 +0300

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = angular.module('flapperNews', []);
 app.controller('MainCtrl', [
 '$scope',
 function($scope){
-  $scope.test = 'Hello world!';
+  $scope.test = 'Hello WoRlD!';
   $scope.posts = [
   'post 1',
   'post 2',
