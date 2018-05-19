b7405f6de3b28f14b216c41beb42abfb17af980c
gevasiliou <ge.vasiliou@gmail.com>
2018-05-19 20:21:15 +0300

    update

--- calculator-tk.py
+++ calculator-tk.py
@@ -5,6 +5,9 @@ from tkinter import messagebox
 calculator = Tk()
 calculator.title("CALCULATOR")
 calculator.resizable(0, 1)#remove or change this in order to get different screen sizes
+fgc = "black" #button foreground color
+bgc = "gray" #button background color
+bow = 10 #button borderwidth value
 
 class Application(Frame):
 	def __init__(self, master, *args, **kwargs):
@@ -27,7 +30,7 @@ class Application(Frame):
 	def calculateExpression(self):#python's calculate function 
 		self.expression = self.display.get()
 		self.expression = self.expression.replace("%", "/ 100")
-
+		self.expression = self.expression.replace("^", "**")
 		try:
 			self.result = eval(self.expression)
 			self.replaceText(self.result)
@@ -36,71 +39,81 @@ class Application(Frame):
 
 	def clearText(self):#clears imput on pressing C on Calculator
 		self.replaceText("0")
-
 	def createWidgets(self):
-		self.display = Entry(self, font=("Helvetica", 66), borderwidth=10, relief=RAISED, justify=RIGHT)
+		self.display = Entry(self, font=("Helvetica", 66), borderwidth=bow,  fg=fgc, bg=bgc, relief=RAISED, justify=RIGHT)
 		self.display.insert(0, "0")
-		self.display.grid(row=0, column=0, columnspan=5)
-
+		self.display.grid(row=0, column=0, columnspan=5) #columnspan=number of columns
 #This is the First Row
 #http://effbot.org/tkinterbook/button.htm
-		self.sevenButton = Button(self, font=("Helvetica", 44), text="7", borderwidth=10, fg="red", bg="black",  command=lambda: self.appendToDisplay("7"))
+		self.sevenButton = Button(self, font=("Helvetica", 44), text="7", borderwidth=bow, fg=fgc, bg=bgc,  command=lambda: self.appendToDisplay("7"))
 		self.sevenButton.grid(row=1, column=0, sticky="NWNESWSE")
 
-		self.eightButton = Button(self, font=("Helvetica", 44), text="8", borderwidth=10, relief="groove", command=lambda: self.appendToDisplay("8"))
+		self.eightButton = Button(self, font=("Helvetica", 44), text="8", borderwidth=bow, fg=fgc, bg=bgc, relief="groove", command=lambda: self.appendToDisplay("8"))
 		self.eightButton.grid(row=1, column=1, sticky="NWNESWSE")
 
-		self.nineButton = Button(self, font=("Helvetica", 44), text="9", borderwidth=10, command=lambda: self.appendToDisplay("9"))
+		self.nineButton = Button(self, font=("Helvetica", 44), text="9", borderwidth=bow, fg=fgc, bg=bgc, command=lambda: self.appendToDisplay("9"))
 		self.nineButton.grid(row=1, column=2, sticky="NWNESWSE")
 
-		self.timesButton = Button(self, font=("Helvetica", 44), text="*", borderwidth=10, command=lambda: self.appendToDisplay("*"))
+		self.timesButton = Button(self, font=("Helvetica", 44), text="*", borderwidth=bow, fg=fgc, bg=bgc, command=lambda: self.appendToDisplay("*"))
 		self.timesButton.grid(row=1, column=3, sticky="NWNESWSE")
 
-		self.clearButton = Button(self, font=("Helvetica", 44), text="C", borderwidth=10, command=lambda: self.clearText())
+		self.clearButton = Button(self, font=("Helvetica", 44), text="C", borderwidth=bow, fg=fgc, bg=bgc, command=lambda: self.clearText())
 		self.clearButton.grid(row=1, column=4, sticky="NWNESWSE")
 
 #This is the Second Row
-		self.fourButton = Button(self, font=("Helvetica", 44), text="4", borderwidth=10, command=lambda: self.appendToDisplay("4"))
+		self.fourButton = Button(self, font=("Helvetica", 44), text="4", borderwidth=bow, fg=fgc, bg=bgc, command=lambda: self.appendToDisplay("4"))
 		self.fourButton.grid(row=2, column=0, sticky="NWNESWSE")
 
-		self.fiveButton = Button(self, font=("Helvetica", 44), text="5", borderwidth=10, command=lambda: self.appendToDisplay("5"))
+		self.fiveButton = Button(self, font=("Helvetica", 44), text="5", borderwidth=bow, fg=fgc, bg=bgc, command=lambda: self.appendToDisplay("5"))
 		self.fiveButton.grid(row=2, column=1, sticky="NWNESWSE")
 
-		self.sixButton = Button(self, font=("Helvetica", 44), text="6", borderwidth=10, command=lambda: self.appendToDisplay("6"))
+		self.sixButton = Button(self, font=("Helvetica", 44), text="6", borderwidth=bow, fg=fgc, bg=bgc, command=lambda: self.appendToDisplay("6"))
 		self.sixButton.grid(row=2, column=2, sticky="NWNESWSE")
 
-		self.divideButton = Button(self, font=("Helvetica", 44), text="/", borderwidth=10, command=lambda: self.appendToDisplay("/"))
+		self.divideButton = Button(self, font=("Helvetica", 44), text="/", borderwidth=bow, fg=fgc, bg=bgc, command=lambda: self.appendToDisplay("/"))
 		self.divideButton.grid(row=2, column=3, sticky="NWNESWSE")
 
-		self.percentageButton = Button(self, font=("Helvetica", 44), text="%", borderwidth=10, command=lambda: self.appendToDisplay("%"))
+		self.percentageButton = Button(self, font=("Helvetica", 44), text="%", borderwidth=bow, fg=fgc, bg=bgc, command=lambda: self.appendToDisplay("%"))
 		self.percentageButton.grid(row=2, column=4, sticky="NWNESWSE")
 
 #This is the Third Row
-		self.oneButton = Button(self, font=("Helvetica", 44), text="1", borderwidth=10, command=lambda: self.appendToDisplay("1"))
+		self.oneButton = Button(self, font=("Helvetica", 44), text="1", borderwidth=bow, fg=fgc, bg=bgc, command=lambda: self.appendToDisplay("1"))
 		self.oneButton.grid(row=3, column=0, sticky="NWNESWSE")
 
-		self.twoButton = Button(self, font=("Helvetica", 44), text="2", borderwidth=10, command=lambda: self.appendToDisplay("2"))
+		self.twoButton = Button(self, font=("Helvetica", 44), text="2", borderwidth=bow, fg=fgc, bg=bgc, command=lambda: self.appendToDisplay("2"))
 		self.twoButton.grid(row=3, column=1, sticky="NWNESWSE")
 
-		self.threeButton = Button(self, font=("Helvetica", 44), text="3", borderwidth=10, command=lambda: self.appendToDisplay("3"))
+		self.threeButton = Button(self, font=("Helvetica", 44), text="3", borderwidth=bow, fg=fgc, bg=bgc, command=lambda: self.appendToDisplay("3"))
 		self.threeButton.grid(row=3, column=2, sticky="NWNESWSE")
 
-		self.minusButton = Button(self, font=("Helvetica", 44), text="-", borderwidth=10, command=lambda: self.appendToDisplay("-"))
+		self.minusButton = Button(self, font=("Helvetica", 44), text="-", borderwidth=bow, fg=fgc, bg=bgc, command=lambda: self.appendToDisplay("-"))
 		self.minusButton.grid(row=3, column=3, sticky="NWNESWSE")
 
-		self.equalsButton = Button(self, font=("Helvetica", 44), text="=", borderwidth=10, command=lambda: self.calculateExpression())
-		self.equalsButton.grid(row=3, column=4, sticky="NWNESWSE", rowspan=2)
+		self.equalsButton = Button(self, font=("Helvetica", 44), text="=", borderwidth=bow, fg=fgc, bg=bgc, command=lambda: self.calculateExpression())
+		self.equalsButton.grid(row=3, column=4, sticky="NWNESWSE", rowspan=3)
 
 #This is the Fourth Row
-		self.zeroButton = Button(self, font=("Helvetica", 44), text="0", borderwidth=10, command=lambda: self.appendToDisplay("0"))
-		self.zeroButton.grid(row=4, column=0, columnspan=2, sticky="NWNESWSE")
+		self.zeroButton = Button(self, font=("Helvetica", 44), text="0", borderwidth=bow, fg=fgc, bg=bgc, command=lambda: self.appendToDisplay("0"))
+		self.zeroButton.grid(row=4, column=0, columnspan=2, sticky="NWNESWSE") #columnspan here determines how many columns the button will occupy
 
-		self.dotButton = Button(self, font=("Helvetica", 44), text=".", borderwidth=10, command=lambda: self.appendToDisplay("."))
+		self.dotButton = Button(self, font=("Helvetica", 44), text=".", borderwidth=bow, fg=fgc, bg=bgc, command=lambda: self.appendToDisplay("."))
 		self.dotButton.grid(row=4, column=2, sticky="NWNESWSE")
 
-		self.plusButton = Button(self, font=("Helvetica", 44), text="+", borderwidth=10, command=lambda: self.appendToDisplay("+"))
+		self.plusButton = Button(self, font=("Helvetica", 44), text="+", borderwidth=bow, fg=fgc, bg=bgc, command=lambda: self.appendToDisplay("+"))
 		self.plusButton.grid(row=4, column=3, sticky="NWNESWSE")
 
+#This is the Fifth-testing-Row
+		self.LeftParButton = Button(self, font=("Helvetica", 44), text="(", borderwidth=bow, fg=fgc, bg=bgc, command=lambda: self.appendToDisplay("("))
+		self.LeftParButton.grid(row=5, column=0, sticky="NWNESWSE")
+
+		self.RightParButton = Button(self, font=("Helvetica", 44), text=")", borderwidth=bow, fg=fgc, bg=bgc, command=lambda: self.appendToDisplay(")"))
+		self.RightParButton.grid(row=5, column=1, sticky="NWNESWSE")
+
+		self.PowButton = Button(self, font=("Helvetica", 44), text="^", borderwidth=bow, fg=fgc, bg=bgc, command=lambda: self.appendToDisplay("^"))
+		self.PowButton.grid(row=5, column=2, sticky="NWNESWSE")
+
+		self.ExitButton = Button(self, font=("Helvetica", 44), text="exit", borderwidth=bow, fg=fgc, bg=bgc, command=lambda: exit())
+		self.ExitButton.grid(row=5, column=3, sticky="NWNESWSE")
 
 app = Application(calculator).grid()		
 calculator.mainloop()
