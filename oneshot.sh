216caa81a3a00b637c3d654e51b9de2d89f54b53
gevasiliou <ge.vasiliou@gmail.com>
2018-12-09 23:24:53 +0200

    update

--- oneshot.sh
+++ oneshot.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
-echo "Shell level parameter SHLVL=$SHLVL";echo "BASH_SUBSHELL=$BASH_SUBSHELL"
+echo "Reading parameters -s and -g using getopt & \$OPTARG"
+echo "\$OPTARG value before getopts: ${OPTARG} - \$OPTIND: $OPTIND"
+while getopts ":s:g:" o; do
+    case $o in
+        s)
+            argss="$OPTARG"
+            ;;
+        g)
+            argsg="$OPTARG"
+            ;;
+    esac
+done
+echo "case \$o in s): \$argss=$argss"
+echo "case \$o in g): \$argsg=$argsg"
+echo "\$OPTARG after getopts: $OPTARG - \$OPTIND=$OPTIND"
+
+echo "Shell level parameter \$SHLVL=$SHLVL";echo "\$BASH_SUBSHELL=$BASH_SUBSHELL"
 echo "Just an echo - nothing more to be done here"
 
-echo "You run this script as \$USER=$USER (this is the value of USER variable)"
+echo "You run this script as \$USER=$USER (this is the value of \$USER variable)"
 echo -n "whoami command returns:  " && whoami
 echo -n "/bin/id returns:  " && id
 echo "Your \$HOME dir is $HOME"
 echo "Your \$PATH is $PATH"
 echo "Numbers of Parameter received= "$#
-echo "special parameter \$0 value: " "$0"
-echo "full location of this script:" $PWD${0#.}
-[[ -z $1 ]] && echo "parameter 1 is empty" ||  echo "Parameter 1 is: $1"
+echo "full location of this script: \$PWD\${0#.}=" $PWD${0#.}
+[[ -z $1 ]] && echo "parameter 1 is empty"
+echo "\$0 is: $0"
+echo "\$1 is: $1"
 echo "\$2 is: $2"
 echo "\$3 is: $3"
+echo "\$4 is: $4"
 printf "%d args found in \$@ array\n" "$#"
 printf " <%s>\n" "$@"  && echo
 #demonstrating word splitting - Test with ./oneshot.sh hello world "how are you?"  --> <hello> <world> <how are you?>
@@ -23,8 +41,10 @@ printf " <%s>\n" "$@"  && echo
 #{ pipedata=$(<mypipe) && echo "$pipedata"; } &
 #pipedata=$(<mypipe)
 
-read -p "Enter a value" 
-echo "you entered $REPLY (this is the value of default REPLY var , used to capture the response of read -p when no var is given)"
+read -p "Enter a value : " 
+echo "you entered $REPLY (this is the value of default \$REPLY var , used to capture the response of read -p when no var is given)"
+
+
 #echo "$pipedata"
 
 #dpkgnum agrep
