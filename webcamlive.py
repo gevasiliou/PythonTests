73553e47ecb24a5d51797460f89092b09f2947a9
gevasiliou <ge.vasiliou@gmail.com>
2018-12-30 13:55:12 +0200

    update

--- webcamlive.py
+++ webcamlive.py
@@ -1,7 +1,7 @@
 import pygame, sys
 import pygame.camera
-#from pygame.locals import *
-
+from pygame.locals import *
+#https://gist.github.com/stuartwesterman/967046abf8f00756089345edd950b6bc
 #pygame.init()
 size = (640,480)
 pygame.camera.init()
@@ -15,6 +15,10 @@ while 1:
     screen.blit(image,(0,0))
     pygame.display.update()
     for event in pygame.event.get():
+        print event #works nicely. Prints on screen all events like mouse move, keydown, key codes, etc
+        #print pygame.event.get() #on the other hand this does NOT work nicely,
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == KEYDOWN and event.key == K_s:
+            pygame.image.save(image, "filename.jpg") #control+s saves the image
 
