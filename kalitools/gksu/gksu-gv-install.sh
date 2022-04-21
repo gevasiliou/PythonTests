8a64e455852e2a659a559fd9a2c1ecc92ec6e259
root <root@debi-local-64>
2022-04-22 02:04:48 +0300

    update

--- kalitools/gksu/gksu-gv-install.sh
+++ kalitools/gksu/gksu-gv-install.sh
@@ -3,8 +3,24 @@
 #cp -i -v "${l##*/}" "$l";
 #done
 #above method abantoned; read-p does not work for some reason inside while read loop
-
 mapfile -t -O1 gkf < <(grep -v '^[#].*' dpkg-l-gksu-files-only.log)
+if [[ $1 == "--remove" ]]; then 
+echo "remove option given";
+for l in "${gkf[@]}" ;do
+printf "prepare to remove $l ";
+read -p " - continue ? [y/n] ? :" an
+[[ $an == "y" ]] && rm -i -v "$l" || echo "===========> skipping removal of $l <============";
+done
+[ -d "/usr/share/doc/gksu" ] && rm -r -i -v /usr/share/doc/gksu;
+[ -d "/usr/share/gksu" ] && rm -r -i -v /usr/share/gksu;
+rm -i -v /usr/share/menu/gksu
+rm -i -v /usr/bin/gksudo 
+exit
+fi
+
+[ ! -d "/usr/share/doc/gksu" ] && mkdir /usr/share/doc/gksu && ls -all -d /usr/share/doc/gksu
+[ ! -d "/usr/share/gksu" ] && mkdir /usr/share/gksu && ls -all -d /usr/share/gksu
+
 for l in "${gkf[@]}" ;do
 printf "prepare to copy ${l##*/} to $l ";
 read -p " - continue ? [y/n] ? :" an
@@ -16,3 +32,6 @@ printf "prepare to copy usr-share-menu-gksu to /usr/share/menu/gksu" && read -p 
 
 printf "prepare to copy gksudo to /usr/bin/gksudo " && read -p " - continue ? [y/n] ? :" an
 [[ $an == "y" ]] && cp -i -v -P gksudo /usr/bin/gksudo || echo "===========> skipping <============";
+unset gkf
+unset l
+
